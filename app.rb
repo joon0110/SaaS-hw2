0291168bfe1da11532dbce14a034f48ea1c18b52
Joon Ahn <sa3883@columbia.edu>
2026-09-24 17:18:32 +0000

    Update greeting

--- app.rb
+++ app.rb
@@ -5,6 +5,6 @@ class MyApp < Sinatra::Base
   set :host_authorization, { permitted_hosts: [] }  
 
   get '/' do
-    '<!DOCTYPE html><html><body><h1>Goodbye 12 World</h1></body></html>'
+    '<!DOCTYPE html><html><body><h1>Goodbye Test World</h1></body></html>'
   end
 end
